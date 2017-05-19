c10443935e1fbd35f33b6347f0353150708dcd11
jayshawn <jayshawn@qq.com>
2017-05-19 20:13:12 +0800

    2017/5/19 20:13

--- src/test/java/jayshawn/hibernate/test/TestNews.java
+++ src/test/java/jayshawn/hibernate/test/TestNews.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.util.List;
+
+import javax.security.auth.Destroyable;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -11,8 +14,10 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.hibernate.service.ServiceRegistry;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import com.mysql.jdbc.*;
 
@@ -20,24 +25,51 @@ import jayshawn.hibernate.helloworld.News;
 
 public class TestNews {
 
+	private Session session = null;
+	private SessionFactory sessionFactory = null;
+	
+	@Before
+	public void init(){
+		// A SessionFactory is set up once for an application!
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+		.configure("/hibernate.cfg.xml") // configures settings from hibernate.cfg.xml
+		.build();
+		sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+	}
+	
+	@After
+	public void Destroyable(){
+		session.getTransaction().commit();
+		session.close();
+	}
 	
 	
 	@Test
-	public void test() {
-		
-		// A SessionFactory is set up once for an application!
-		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-		.configure() // configures settings from hibernate.cfg.xml
-		.build();
-		
-		SessionFactory sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
-		Session session = sessionFactory.openSession();
-		System.out.println(session);
-		session.beginTransaction();
-		session.save( new News("AA","AA",new Timestamp(System.currentTimeMillis())));
+	public void testSave() {
+		session.save( new News("BB","BB",new Timestamp(System.currentTimeMillis())));
+	}
+	
+	@Test
+	public void testGetCache(){
+		//只发送了一条查询语句, 因为启用了ehcache,二级缓存是sessionFactory级别的,因为sessionFactory没有关闭,所以生效了
+		News news1 = session.get(News.class, 1);
 		session.getTransaction().commit();
 		session.close();
-		
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		News new2 = session.get(News.class, 1);
+	}
+	
+	@Test
+	public void testQueryCache(){
+		Query query =  session.createQuery("FROM News");
+		query.setCacheable(true);
+		List<News> list = query.list();
+		System.out.println(list.size());
+		list = query.list();
+		System.out.println(list.size());
 		
 	}
 
